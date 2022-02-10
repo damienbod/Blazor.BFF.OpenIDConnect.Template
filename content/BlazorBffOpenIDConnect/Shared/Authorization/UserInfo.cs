0612fbc1d164b33550a939ce3c0604b14b8b7c43
damienbod <damien_bod@hotmail.com>
2022-02-10 21:17:14 +0100

    styling

--- content/BlazorBffOpenIDConnect/Shared/Authorization/UserInfo.cs
+++ content/BlazorBffOpenIDConnect/Shared/Authorization/UserInfo.cs
@@ -4,7 +4,7 @@ namespace BlazorBffOpenIDConnect.Shared.Authorization;
 
 public class UserInfo
 {
-    public static readonly UserInfo Anonymous = new UserInfo();
+    public static readonly UserInfo Anonymous = new();
 
     public bool IsAuthenticated { get; set; }
 
