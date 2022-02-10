f0d0d4e1054cdd66d3158574cd58af096ffb9574
damienbod <damien_bod@hotmail.com>
2022-02-10 21:15:42 +0100

    clean up namespaces

--- content/BlazorBffOpenIDConnect/Server/Startup.cs
+++ content/BlazorBffOpenIDConnect/Server/Startup.cs
@@ -10,94 +10,93 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
-namespace BlazorBffOpenIDConnect.Server
+namespace BlazorBffOpenIDConnect.Server;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAntiforgery(options =>
         {
-            Configuration = configuration;
+            options.HeaderName = "X-XSRF-TOKEN";
+            options.Cookie.Name = "__Host-X-XSRF-TOKEN";
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        });
+
+        services.AddHttpClient();
+        services.AddOptions();
+
+        var openIDConnectSettings = Configuration.GetSection("OpenIDConnectSettings");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddOpenIdConnect(options =>
+        {
+            options.SignInScheme = "Cookies";
+            options.Authority = openIDConnectSettings["Authority"];
+            options.ClientId = openIDConnectSettings["ClientId"];
+            options.ClientSecret = openIDConnectSettings["ClientSecret"];
+            options.RequireHttpsMetadata = true;
+            options.ResponseType = OpenIdConnectResponseType.Code;
+            options.UsePkce = true;
+            options.Scope.Add("profile");
+            options.SaveTokens = true;
+            options.GetClaimsFromUserInfoEndpoint = true;
+        });
+
+        services.AddControllersWithViews(options =>
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+
+        services.AddRazorPages().AddMvcOptions(options =>
+        {
+            //var policy = new AuthorizationPolicyBuilder()
+            //    .RequireAuthenticatedUser()
+            //    .Build();
+            //options.Filters.Add(new AuthorizeFilter(policy));
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseSecurityHeaders(
+            SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment(),
+                Configuration["OpenIDConnectSettings:Authority"]));
 
-        public void ConfigureServices(IServiceCollection services)
+        app.UseHttpsRedirection();
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddAntiforgery(options =>
-            {
-                options.HeaderName = "X-XSRF-TOKEN";
-                options.Cookie.Name = "__Host-X-XSRF-TOKEN";
-                options.Cookie.SameSite = SameSiteMode.Strict;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-            });
-
-            services.AddHttpClient();
-            services.AddOptions();
-
-            var openIDConnectSettings = Configuration.GetSection("OpenIDConnectSettings");
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            })
-           .AddCookie()
-           .AddOpenIdConnect(options =>
-           {
-               options.SignInScheme = "Cookies";
-               options.Authority = openIDConnectSettings["Authority"];
-               options.ClientId = openIDConnectSettings["ClientId"];
-               options.ClientSecret = openIDConnectSettings["ClientSecret"];
-               options.RequireHttpsMetadata = true;
-               options.ResponseType = OpenIdConnectResponseType.Code;
-               options.UsePkce = true;
-               options.Scope.Add("profile");
-               options.SaveTokens = true;
-               options.GetClaimsFromUserInfoEndpoint = true;
-           });
-
-            services.AddControllersWithViews(options =>
-                 options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
-
-            services.AddRazorPages().AddMvcOptions(options =>
-            {
-                //var policy = new AuthorizationPolicyBuilder()
-                //    .RequireAuthenticatedUser()
-                //    .Build();
-                //options.Filters.Add(new AuthorizeFilter(policy));
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseSecurityHeaders(
-                SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment(),
-                    Configuration["OpenIDConnectSettings:Authority"]));
-
-            app.UseHttpsRedirection();
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-                endpoints.MapFallbackToPage("/_Host");
-            });
-        }
+            endpoints.MapRazorPages();
+            endpoints.MapControllers();
+            endpoints.MapFallbackToPage("/_Host");
+        });
     }
 }
