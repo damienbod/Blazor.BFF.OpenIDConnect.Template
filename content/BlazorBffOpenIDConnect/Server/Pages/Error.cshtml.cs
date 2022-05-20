1aa03331b9d8cbb7c9ef9dfbe294c526b70b09a7
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2022-05-20 22:13:43 +0200

    cleanup

--- content/BlazorBffOpenIDConnect/Server/Pages/Error.cshtml.cs
+++ content/BlazorBffOpenIDConnect/Server/Pages/Error.cshtml.cs
@@ -13,5 +13,8 @@ public class ErrorModel : PageModel
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+    private readonly ILogger<ErrorModel> _logger;
+
+    public ErrorModel(ILogger<ErrorModel> logger) => _logger = logger;
     public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
 }
