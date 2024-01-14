558978162f1b5ddaed8d1b55c610e37458c0f732
damienbod <damien_bod@hotmail.com>
2024-01-14 11:06:38 +0100

    Update security headers

--- BlazorBffOpenIdConnect/Server/SecurityHeadersDefinitions.cs
+++ BlazorBffOpenIdConnect/Server/SecurityHeadersDefinitions.cs
@@ -2,15 +2,17 @@
 
 public static class SecurityHeadersDefinitions
 {
-    public static HeaderPolicyCollection GetHeaderPolicyCollection(bool isDev, string idpHost)
+    public static HeaderPolicyCollection GetHeaderPolicyCollection(bool isDev, string? idpHost)
     {
+        ArgumentNullException.ThrowIfNull(idpHost);
+
         var policy = new HeaderPolicyCollection()
             .AddFrameOptionsDeny()
             .AddContentTypeOptionsNoSniff()
             .AddReferrerPolicyStrictOriginWhenCrossOrigin()
             .AddCrossOriginOpenerPolicy(builder => builder.SameOrigin())
             .AddCrossOriginResourcePolicy(builder => builder.SameOrigin())
-            .AddCrossOriginEmbedderPolicy(builder => builder.RequireCorp()) // remove for dev if using hot reload
+            .AddCrossOriginEmbedderPolicy(builder => builder.RequireCorp())
             .AddContentSecurityPolicy(builder =>
             {
                 builder.AddObjectSrc().None();
@@ -24,12 +26,9 @@ public static class SecurityHeadersDefinitions
 
                 // due to Blazor
                 builder.AddScriptSrc()
-                    .Self()
-                    .WithHash256("v8v3RKRPmN4odZ1CWM5gw80QKPCCWMcpNeOmimNL2AA=")
+                    // .Self() Add this if you want to use the visual studio debugging tools
+                    .WithNonce()
                     .UnsafeEval();
-
-                // disable script and style CSP protection if using Blazor hot reload
-                // if using hot reload, DO NOT deploy with an insecure CSP
             })
             .RemoveServerHeader()
             .AddPermissionsPolicy(builder =>
